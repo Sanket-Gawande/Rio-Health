2d2b92dc37db18daae961d3731013b4a55c55835
sanketg <sanketgawande.gcoey@gmail.com>
2022-09-28 09:16:56 +0530

    final build

---
+++ index.js
@@ -0,0 +1,35 @@
+
+import http from "http";
+import data from "./data-store.json" assert { type: "json" };
+const app = http.createServer();
+app.on("request", (req, res) => {
+  const reqParams = req.url.split("/");
+  //   bad request if id is not present in url
+  if (reqParams[1] === "projects" && reqParams.length == 2) {
+    res.statusCode = 400;
+    res.end(JSON.stringify({ message: "BAD REQUEST" }));
+  }
+  //   checking for data if id is present
+  if (
+    reqParams.length === 3 &&
+    req.method === "GET" &&
+    reqParams[1] === "projects"
+  ) {
+    const project = data.filter((item) => item.id == reqParams[2])[0];
+    if (project) {
+      res.statusCode = 200;
+      res.end(JSON.stringify(project));
+      return;
+    } else {
+      res.statusCode = 404;
+      res.end();
+      return;
+    }
+  }
+  res.statusCode = 404;
+  res.end();
+});
+
+app.listen(8000, (error) => {
+  console.log({ error, message: "server is running on port 8000" });
+});
